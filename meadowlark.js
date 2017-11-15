bdcca6d8c08e7ec5609e9bc1c2ce963195db278c
jonnypeng <jonny.peng@qq.com>
2017-11-15 22:39:19 +0800

    完成基本的页面

--- meadowlark.js
+++ meadowlark.js
@@ -2,22 +2,43 @@ var express = require('express');
 
 var app = express();
 
-app.set('port',process.env.PORT || 6000);
+var handlebars = require('express3-handlebars')
+.create({defaultLayout:'main'});
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+app.set('port',process.env.PORT || 3000);
+
+app.get('/',function (req,res){
+	//res.type('text/plain');
+	//res.send('Meadowlark Travel');
+	res.render('home');
+});
+
+app.get('/about*',function (req,res){
+	//res.type('text/plain');
+	//res.send('About Meadowlark Travel');
+	res.render('about');
+});
 
 app.use(function (req,res){
-	res.type('text/plain');
+	//res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
+	//res.send('404 - Not Found');
 });
 
 app.use(function (err,req,res,next){
 	console.error(err.stack);
-	res.type('text/plain');
+	//res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
+	//res.send('500 - Server Error');
 });
 
 app.listen(app.get('port'),function (){
 	console.log('Express started on http://localhost:' + app.get('port') + ';press Ctrl-C to terminate');
 });
+
 
